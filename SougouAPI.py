b7269860c4cebe7307916903082f04c24a289942
joshua zou <34274535+jdstkxx@users.noreply.github.com>
2018-04-10 00:01:54 +0800

    Update SougouAPI.py

--- SougouAPI.py
+++ SougouAPI.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 # 搜狗API字典
 
@@ -16,22 +15,4 @@ SougouAPI={
     },
     
 }
-=======
-# -*- coding: utf-8 -*-
-# 搜狗API字典
 
-SougouAPI={
-    #基本文本分析API
-    "ocr":    {
-        'APINAME':'图像识别', #API中文简称
-        'APIDESC': '识别图像中的文字', #API描述
-        'APIURL': 'http://api.ai.sogou.com/pub/ocr' #API请求URL
-    },
-    "idcard":{
-        'APINAME':'身份证识别', #API中文简称
-        'APIDESC': '身份证识别', #API描述
-        'APIURL': 'http://api.ai.sogou.com/pub/ocr/idcard' #API请求URL
-    },
-    
-}
->>>>>>> origin/master
